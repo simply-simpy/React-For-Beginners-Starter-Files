2a416e3f4aaadd25b025b7bae03dbbbc2301d023
scott simpson <scott@scott.local>
2017-03-07 14:47:43 -0800

    added all components

---
+++ catch-of-the-day/src/components/Order.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Order extends  React.Component {
+    render(){
+        return(
+            <div className="order">
+                This is order
+            </div>
+        )
+    }
+}
+export default Order;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,9 +1,10 @@
 const main = document.querySelector('#main');
+
 import React from 'react';
 import { render } from 'react-dom'; // import one method from react-dom
 import './css/style.css';
-
+import App from './components/App';
 
 import StorePicker from './components/StorePicker';
 
-render(<StorePicker/>, main);
+render(<App/>, main);
